a3a0fbd84f0d0c23f60c9ad3690e4c670faf20da
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-12 13:18:23 -0400

    created a test file and added the first test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+afterEach(rtl.cleanup);
+
+test ('renders without crashing',  () => {
+   rtl.render(<App />) //passing a component to render
+})
+
+
